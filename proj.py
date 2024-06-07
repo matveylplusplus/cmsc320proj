1a17bd7d9ca8cc920a995fe5ece21cb9aa92acc1
SSD Matt <matthecrazyguy@gmail.com>
2024-06-07 15:25:29 -0400

    tested out imhe dataset

--- proj.py
+++ proj.py
@@ -3,9 +3,16 @@ import pandas as pd
 
 
 def main():
-    xlsx = pd.ExcelFile("Inflation-data.xlsx")
-    df = pd.read_excel(xlsx, "hcpi_m")
-    print(df[197001])
+    # xlsx = pd.ExcelFile("WDIEXCEL.xlsx")
+    # df1 = pd.read_excel(xlsx, "Data")
+
+    df2 = pd.read_csv("datasets/ihme/src/ihme.csv")
+    print(
+        df2[
+            (df2["location_name"] == "United States of America")
+            & (df2["cause_name"] == "Mental disorders")
+        ][["cause_name", "year", "val", "upper", "lower"]]
+    )
 
 
 if __name__ == "__main__":
