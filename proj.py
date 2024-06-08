0ec3d168ec2ba169a6f273bff2374183c21abac3
SSD Matt <matthecrazyguy@gmail.com>
2024-06-07 21:19:59 -0400

    tested some more datasets

--- proj.py
+++ proj.py
@@ -3,9 +3,15 @@ import pandas as pd
 
 
 def main():
-    # xlsx = pd.ExcelFile("WDIEXCEL.xlsx")
-    # df1 = pd.read_excel(xlsx, "Data")
+    xlsx = pd.ExcelFile("datasets/wdi/src/WDIEXCEL.xlsx")
+    df1 = pd.read_excel(xlsx, "Data")
+    print(
+        df1[df1["Indicator Name"] == "Gini index"].drop(
+            ["Country Name", "Indicator Code"], axis=1
+        )
+    )
 
+    """
     df2 = pd.read_csv("datasets/ihme/src/ihme.csv")
     print(
         df2[
@@ -13,6 +19,7 @@ def main():
             & (df2["cause_name"] == "Mental disorders")
         ][["cause_name", "year", "val", "upper", "lower"]]
     )
+    """
 
 
 if __name__ == "__main__":
